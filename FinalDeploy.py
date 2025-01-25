fa400f8c378230080efa4ead377404dd5b070bbb
IshanOstwal <ishan_o@ee.iitr.ac.in>
2025-01-25 22:36:58 +0530

    The Deploy File

---
+++ FinalDeploy.py
@@ -0,0 +1,19 @@
+from mira_sdk import MiraClient, CompoundFlow
+from mira_sdk.exceptions import FlowError
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+api_key = os.getenv("MIRA_API_KEY")
+
+# Initialize the client
+client = MiraClient(config={"API_KEY": api_key})
+
+# Basic test
+flow = CompoundFlow(source=r"D:\Coding\Python\Flows(YAML)\shortFilmGen.yaml")  # Load flow configuration
+try:
+        client.flow.deploy(flow)  # Exit loop if successful
+        print("Deployed succesffuly")                             # Deploy to platform
+except FlowError as e:
+    print(f"Error occurred: {str(e)}")                      # Handle deployment error
