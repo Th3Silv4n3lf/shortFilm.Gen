5c368253925ce235fde4922e36171dcfb152af2c
IshanOstwal <ishan_o@ee.iitr.ac.in>
2025-01-25 22:29:54 +0530

    Testing File
    Adding the main testing file for python

---
+++ FinalTest.py
@@ -0,0 +1,26 @@
+from mira_sdk import MiraClient, CompoundFlow
+from mira_sdk.exceptions import FlowError
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+api_key = os.getenv("MIRA_API_KEY")
+
+# Initialize the client
+client = MiraClient(config={"API_KEY": api_key})
+
+# Basic test
+flow = CompoundFlow(source=r"D:\Coding\Python\Flows(YAML)\shortFilmGen.yaml")  # Load flow configuration
+input_dict = {"genre": "Suspense Action", "cinematographer": "Quentin Tarantino", "story_type": "KGF, Bahubali"}  # Prepare test input
+
+#Exception Handling
+try:
+    response = client.flow.test(flow, input_dict)  # Test entire pipeline
+    print("Test response:", response)
+    
+    # Save the response to a file
+    with open("response_output.txt", "w") as file:
+        file.write(str(response))
+except FlowError as e:
+    print("Test failed:", str(e))
